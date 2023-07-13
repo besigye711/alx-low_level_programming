b4cc0c76c74c7bd3bc1e77dd3b2a37d5e80e4ae4
“besigye711” <besigyeabias711@gmail.com>
2023-07-12 23:22:17 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main -A program that prints a line using the printf
   *return:0(success)
   */
-int main(){
+int main(void){
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
