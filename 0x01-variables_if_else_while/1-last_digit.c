bf55f36d4bfe3f43d6c90901c889e8e72ff6aa48
“besigye711” <besigyeabias711@gmail.com>
2023-07-17 10:43:43 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
@@ -23,6 +23,6 @@ int main(void)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 
 	return (0);
-}                                                                                                                               
+}                                                                                                                            
 ~                                                                                                                                                                      
 ~                                                i
