a1d6c70c3b05208df91c32208ba9cbfc68c6c949
“besigye711” <besigyeabias711@gmail.com>
2023-07-17 07:14:00 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<>
+/**
+* main - A program that prints the alphabet in lowercase,
+*followed by a new line
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
