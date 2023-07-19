5b07f2f5846e1922e82785b209c14ead2d707184
“besigye711” <besigyeabias711@gmail.com>
2023-07-19 00:11:20 -0700

    tasks

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * main - tests function that prints if the integer is either positive or negative
+ * Return: o
+ */
+int main( void)
+{
+	int i;
+	i = 98;
+	positive_or_negative(i);
+	return (0);
